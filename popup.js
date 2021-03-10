da022eed8ead8beff9d66c032d1cca69a39ebff0
cuzic <shopping@cuzic.com>
2021-03-10 09:06:04 +0900

    add debug

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
+function debug(message){
+  const elem = document.getElementById("debug")
+  elem.innerHTML += `${JSON.stringify(message)}<br>\n`
+}
+
 function sendMessage(message, callback){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     chrome.tabs.sendMessage(tabs[0].id, message, callback)
